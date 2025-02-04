995b31821725606d820ba97963b986178b65f6a2
Azeez Salu <azeez.salu@oddball.io>
2025-02-04 09:29:20 -0500

    updated code

--- terraform/dynamic-eks/infrastructure/eks-cluster/eks-cluster.tf
+++ terraform/dynamic-eks/infrastructure/eks-cluster/eks-cluster.tf
@@ -113,6 +113,8 @@ resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "kube-proxy"
   addon_version     = "v1.31.2-eksbuild.3"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # Configure the VPC CNI addon for networking
@@ -121,6 +123,8 @@ resource "aws_eks_addon" "vpc_cni" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "vpc-cni"
   addon_version     = "v1.19.0-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # Configure the CoreDNS addon for DNS resolution
@@ -129,4 +133,6 @@ resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "coredns"
   addon_version     = "v1.11.3-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
