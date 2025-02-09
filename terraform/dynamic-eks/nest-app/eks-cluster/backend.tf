34f03c6a2e39dc71bdc4c02e9baffb8f01ea07ed
Azeez Salu <aosnotes77@gmail.com>
2025-02-08 21:13:45 -0500

    started deleting

--- terraform/dynamic-eks/nest-app/eks-cluster/backend.tf
+++
@@ -1,9 +0,0 @@
-# Configure Terraform to use S3 for state storage and DynamoDB for state locking
-terraform {
-  backend "s3" {
-    bucket         = "aosnote-terraform-remote-state"
-    key            = "nest-app/eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
-  }
-}
