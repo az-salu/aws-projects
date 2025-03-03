b218d05e27498080eb3f4569d630a8f94ba8be2c
Azeez Salu <azeez.salu@oddball.io>
2025-03-02 23:35:25 -0500

    run terraform destroy

--- terraform/dynamic-ecs/nest-app/providers.tf
+++ terraform/dynamic-ecs/nest-app/providers.tf
@@ -1,6 +1,7 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
   region  = local.region
+  
   default_tags {
     tags = {
       "Automation"  = "terraform"
