7aacf5c4ac33fe71df93043fe5c8f9f2759c568d
Azeez Salu <azeez.salu@oddball.io>
2025-03-10 04:22:48 -0400

    update code

--- docker/dynamic-app/nest-app/push-image.ps1
+++ docker/dynamic-app/nest-app/push-image.ps1
@@ -4,19 +4,22 @@ $region = "us-east-1"
 $accountId = "651783246143"
 
 # Attempt to describe the repository
-try {
-    $repoExists = aws ecr describe-repositories --repository-names $repoName --region $region
+$repositoryCheck = aws ecr describe-repositories --repository-names "$repoName" --region "$region" 2>$null
+$repoExists = $?  # $? contains True if previous command succeeded, False otherwise
+
+# Check if the repository exists
+if ($repoExists) {
     Write-Output "Repository already exists. Skipping creation."
-} catch {
+} else {
     Write-Output "Repository does not exist. Creating repository..."
-    aws ecr create-repository --repository-name $repoName --region $region
+    aws ecr create-repository --repository-name "$repoName" --region "$region"
 }
 
 # Tag the Docker image with the ECR repository URI
-docker tag $repoName "$accountId.dkr.ecr.$region.amazonaws.com/$repoName"
+docker tag "$repoName" "$accountId.dkr.ecr.$region.amazonaws.com/$repoName"
 
 # Retrieve an authentication token and log in to the ECR registry
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin "$accountId.dkr.ecr.$region.amazonaws.com"
+aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$accountId.dkr.ecr.$region.amazonaws.com"
 
 # Push the Docker image to the ECR repository
 docker push "$accountId.dkr.ecr.$region.amazonaws.com/$repoName"
