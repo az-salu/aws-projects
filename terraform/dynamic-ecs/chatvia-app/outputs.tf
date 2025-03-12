633e97a6621cbc1dfb12ebeee0fb71b240a033a7
Azeez Salu <azeez.salu@oddball.io>
2025-03-12 04:04:02 -0400

    update chatvia code

--- terraform/dynamic-ecs/chatvia-app/outputs.tf
+++ terraform/dynamic-ecs/chatvia-app/outputs.tf
@@ -1,10 +1,10 @@
 # Output variables to expose infrastructure resources for deployment automation and reference
 output "image_name" {
-  value = module.nest-app.image_name
+  value = module.chatvia-app.image_name
 }
 
 output "image_tag" {
-  value = module.nest-app.image_tag
+  value = module.chatvia-app.image_tag
 }
 
 output "domain_name" {
@@ -12,19 +12,19 @@ output "domain_name" {
 }
 
 output "rds_endpoint" {
-  value = module.nest-app.rds_endpoint
+  value = module.chatvia-app.rds_endpoint
 }
 
 output "ecs_task_definition_name" {
-  value = module.nest-app.ecs_task_definition_name
+  value = module.chatvia-app.ecs_task_definition_name
 }
 
 output "ecs_cluster_name" {
-  value = module.nest-app.ecs_cluster_name
+  value = module.chatvia-app.ecs_cluster_name
 }
 
 output "ecs_service_name" {
-  value = module.nest-app.ecs_service_name
+  value = module.chatvia-app.ecs_service_name
 }
 output "website_url" {
   value = join("", ["https://", local.record_name, ".", local.domain_name])
