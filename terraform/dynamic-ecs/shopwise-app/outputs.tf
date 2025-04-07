f9d7fa5e4f7b013f0d096718f144c3a4724519db
Azeez Salu <azeez.salu@oddball.io>
2025-04-07 03:04:54 -0400

    update code

--- terraform/dynamic-ecs/shopwise-app/outputs.tf
+++ terraform/dynamic-ecs/shopwise-app/outputs.tf
@@ -1,10 +1,10 @@
 # Output variables to expose infrastructure resources for deployment automation and reference
 output "image_name" {
-  value = module.nest-app.image_name
+  value = module.shopwise-app.image_name
 }
 
 output "image_tag" {
-  value = module.nest-app.image_tag
+  value = module.shopwise-app.image_tag
 }
 
 output "domain_name" {
@@ -12,19 +12,19 @@ output "domain_name" {
 }
 
 output "rds_endpoint" {
-  value = module.nest-app.rds_endpoint
+  value = module.shopwise-app.rds_endpoint
 }
 
 output "ecs_task_definition_name" {
-  value = module.nest-app.ecs_task_definition_name
+  value = module.shopwise-app.ecs_task_definition_name
 }
 
 output "ecs_cluster_name" {
-  value = module.nest-app.ecs_cluster_name
+  value = module.shopwise-app.ecs_cluster_name
 }
 
 output "ecs_service_name" {
-  value = module.nest-app.ecs_service_name
+  value = module.shopwise-app.ecs_service_name
 }
 output "website_url" {
   value = join("", ["https://", local.record_name, ".", local.domain_name])
