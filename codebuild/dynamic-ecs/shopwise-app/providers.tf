32a85c76c4fa40d4f3b0a01b5262e8ec607b2183
Azeez Salu <azeez.salu@oddball.io>
2025-04-07 01:56:58 -0400

    update code

---
+++ codebuild/dynamic-ecs/shopwise-app/providers.tf
@@ -0,0 +1,12 @@
+# Configure AWS provider to establish a secure connection between Terraform and AWS
+provider "aws" {
+  region = local.region
+
+  default_tags {
+    tags = {
+      "Automation"  = "terraform"
+      "Project"     = local.project_name
+      "Environment" = local.environment
+    }
+  }
+}
